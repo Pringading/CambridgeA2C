6f1a30a1746a493c95a035d95a9e7ea36eb6adcc
Jessica Marcell <marcellj@cityschools.org.uk>
2025-07-14 11:09:51 +0100

    change format of date in dict_from_candidates function and add docstrings

--- src/read_csv.py
+++ src/read_csv.py
@@ -1,9 +1,18 @@
 from csv import DictReader
 from copy import deepcopy
+from datetime import datetime
+
 
 def get_csv_data(filepath: str) -> list:
-    """Gets data from csv file and returns list of dictionaries"""
-    
+    """Gets data from csv file and returns list of dictionaries
+
+    Args:
+        filepath(str): relative filepath to csv file.
+
+    Returns: list of dictionaries, one dictionary for each row, with the column
+        headings as the keys and the data as the values.
+    """
+
     with open(filepath, 'r', encoding='utf-8-sig') as f:
         data = DictReader(f)
         return list(data)
@@ -11,23 +20,24 @@ def get_csv_data(filepath: str) -> list:
 
 def dict_from_candidates(c_list: list) -> dict:
     """Takes list of dictionaries with candidate numbers and returns dictionary
-    with candidate numbers as keys and UCI and DOB on keys in nested 
+    with candidate numbers as keys and UCI and DOB on keys in nested
     dictionary.
 
     Args:
         c_list: list of dictionaries with candidate information on
             "Candidate Number", "UCI" and "Date of Birth" keys
-        
+
     Returns:
-        dictionary with candidate numbers as keys pointing to nested 
+        dictionary with candidate numbers as keys pointing to nested
         dictionary with "UCI" and "DOB" keys.
 
-    
     Example:
         c_list = [{
-            "Candidate Number": 1000, 
-            "UCI": 1000000251000A, 
-            "Date of Birth": "01/01/2020"}, {}...]
+            "Candidate Number": 1000,
+            "UCI": 1000000251000A,
+            "Date of Birth": "01/01/2020"}, {}...
+        ]
+
         >>> dict_from_candidates(c_list)
         >>> {
             1000: {"UCI": 100000251000A, "DOB": "2020-01-01"},
@@ -37,14 +47,29 @@ def dict_from_candidates(c_list: list) -> dict:
     c_dict = {}
     for c in c_list:
         c_num = int(c["Candidate Number"])
+        parsed_date = datetime.strptime(c["Date of Birth"], r"%d/%m/%Y")
+        formatted_date = datetime.strftime(parsed_date, r"%Y-%m-%d")
         c_dict[c_num] = {
             "UCI": c["UCI"],
-            "DOB": c["Date of Birth"]
+            "DOB": formatted_date
         }
     return c_dict
 
 
 def results_and_candidates(candidates: dict, results: list) -> list:
+    """Adds DOB and UCI to list of results using inputted candidates dictionary
+    and outputs as new list.
+
+    Args: candidates(dict): candidate numbers as keys pointing to dictionary
+            with "DOB" key with candidate's Date of Birth
+            and "UCI" key with candidate's UCI
+
+        results(list): list of dictionaries each with "CandidateNumber" key
+
+    Returns:
+        New list with the same data as the input results list but with
+            additional "DOB" and "UCI" keys
+    """
     data = []
     for result in results:
         candidate = result["CandidateNumber"]
--- test/test_read_csv.py
+++ test/test_read_csv.py
@@ -21,7 +21,7 @@ class TestGetData:
         candidates = get_csv_data(test_file)
         for candidate in candidates:
             assert isinstance(candidate, dict)
-    
+
     @pytest.mark.it("List is expected length")
     def test_list_is_expected_length(self):
         test_file = "data/candidates.csv"
@@ -45,19 +45,19 @@ class TestDictFromCandidates:
         test_file = "data/candidates.csv"
         candidates = get_csv_data(test_file)
         return candidates
-    
+
     @pytest.mark.it("Returns dictionary")
     def test_returns_dict(self, c_list):
         c_dict = dict_from_candidates(c_list)
         assert isinstance(c_dict, dict)
-    
+
     @pytest.mark.it("All candidates in input list are keys in dict")
     def test_all_candidates_keys_in_dict(self, c_list):
         c_dict = dict_from_candidates(c_list)
         for c in c_list:
             c_num = int(c["Candidate Number"])
             assert c_num in c_dict
-    
+
     @pytest.mark.it("All UCIs in dict on candidate key")
     def test_all_ucis_in_dict_on_key(self, c_list):
         c_dict = dict_from_candidates(c_list)
@@ -65,14 +65,17 @@ class TestDictFromCandidates:
             c_num = int(c["Candidate Number"])
             expected_uci = c["UCI"]
             assert c_dict[c_num]["UCI"] == expected_uci
-        
+
     @pytest.mark.it("All date of births in dict on candidate key")
     def test_all_dob_in_dict_on_key(self, c_list):
         c_dict = dict_from_candidates(c_list)
         for c in c_list:
             c_num = int(c["Candidate Number"])
             expected_dob = c["Date of Birth"]
-            assert c_dict[c_num]["DOB"] == expected_dob
+            returned_dob = c_dict[c_num]["DOB"]
+            expected_dob[6:10] == returned_dob[0:4]
+            expected_dob[3:5] == returned_dob[5:7]
+            expected_dob[0:2] == returned_dob[8:10]
 
 
 @pytest.mark.it("Testing results_and_candidates function")
@@ -93,29 +96,47 @@ class TestResultsAndCandidates:
     def test_returns_list(self, test_args):
         results = results_and_candidates(**test_args)
         assert isinstance(results, list)
-    
+
     @pytest.mark.it("Returns list of dicts")
     def test_returns_list_of_dicts(self, test_args):
         results = results_and_candidates(**test_args)
         for result in results:
             assert isinstance(result, dict)
-    
+
     @pytest.mark.it("Returns new list")
     def test_returns_new_list(self, test_args):
         input_list = test_args["results"]
         output_list = results_and_candidates(**test_args)
         assert input_list is not output_list
-    
+
     @pytest.mark.it("New list has dicts with UCI and DOB keys")
     def test_adds_uci_and_dob_keys(self, test_args):
         output_list = results_and_candidates(**test_args)
         for result in output_list:
             assert "UCI" in result
             assert "DOB" in result
-    
+
     @pytest.mark.it("Input list is not mutated")
     def test_input_list_not_mutated(self, test_args):
         sheets = get_worksheets("data/Testing.xlsx")
         original_input = get_results(sheets)
         results_and_candidates(**test_args)
         assert test_args["results"] == original_input
+
+    @pytest.mark.it("Correct data on existing keys")
+    def test_correct_data_on_existing_keys(self, test_args):
+        input_list = test_args["results"]
+        output_list = results_and_candidates(**test_args)
+        for i, result in enumerate(input_list):
+            result["Results"] == output_list[i]["Results"]
+            result["CandidateNumber"] == output_list[i]["CandidateNumber"]
+            result["CandidateName"] == output_list[i]["CandidateName"]
+
+    @pytest.mark.it("Correct data on new keys")
+    def test_correct_data_on_new_keys(self, test_args):
+        output_list = results_and_candidates(**test_args)
+        c_dict = test_args["candidates"]
+        for result in output_list:
+            candidate = result["CandidateNumber"]
+            assert result["UCI"] == c_dict[candidate]["UCI"]
+            assert result["DOB"] == c_dict[candidate]["DOB"]
