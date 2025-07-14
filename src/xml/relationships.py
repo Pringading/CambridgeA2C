271bc87a212e297e791a81fa7de6a55665839ec8
Jessica Marcell <marcellj@cityschools.org.uk>
2025-07-14 14:25:55 +0100

    implement and test get_other_relationships function

--- src/xml/relationships.py
+++ src/xml/relationships.py
@@ -1,4 +1,15 @@
 def get_relationships(pupils: list, organisations: list, date: str) -> list:
+    """Returns list of dictionaries linking UCI numbers to JCQ, exam board id
+    and centre number.
+
+    Args: pupils(list): list of dictionaries each representing a pupil each
+            has a UCI key.
+        organisations (list): list of organisations
+            ie. ["JCQ", centre number (int), exam board id (str)]
+            eg. ["JCQ", 10000, "02"]
+        date: date results are valid from in yyyy-mm-dd format
+    """
+
     relationships = []
     for pupil in pupils:
         for org in organisations:
@@ -12,5 +23,22 @@ def get_relationships(pupils: list, organisations: list, date: str) -> list:
     return relationships
 
 
-def get_other_relationships():
-    pass
+def get_other_relationships(centre: int, exam_board: str, date: str) -> list:
+    """Returns list of dictionaries linking exam board and centre number to JCQ
+
+    Args:
+        centre (int): centre number
+        exam_board (str): exam_board id "02" for cambridge
+        date (str): date results are valid from in yyyy-mm-dd format
+    """
+    relationships = []
+    orgs = [centre, exam_board]
+    for org in orgs:
+        relationships.append({
+                "PartyRelationship_ID": {
+                    "Party_Id_1st": "JCQ",
+                    "Party_Id_2nd": org
+                },
+                "Party_Relationship_Eff_Date": date
+            })
+    return relationships
--- test/test_xml/test_relationships.py
+++ test/test_xml/test_relationships.py
@@ -29,7 +29,7 @@ class TestGetRelationships:
     def test_returns_list(self, test_args):
         relationships = get_relationships(**test_args)
         assert isinstance(relationships, list)
-    
+
     @pytest.mark.it("Returns list of dictionaries")
     def test_returns_list_of_dicts(self, test_args):
         relationships = get_relationships(**test_args)
@@ -38,7 +38,7 @@ class TestGetRelationships:
 
     @pytest.mark.it("Returns list of expected length")
     def test_returns_list_of_expected_length(self, test_args):
-        expected_length = len(test_args["pupils"]) 
+        expected_length = len(test_args["pupils"])
         expected_length *= len(test_args["organisations"])
         relationships = get_relationships(**test_args)
         assert len(relationships) == expected_length
@@ -63,4 +63,38 @@ class TestGetRelationships:
 
 @pytest.mark.it('Testing get_other_relationships function')
 class TestGetOtherRelationships:
-    pass
+    @pytest.fixture
+    def test_args(self):
+        return {
+            "centre": 10000,
+            "exam_board": "02",
+            "date": "2025-07-07"
+        }
+
+    @pytest.mark.it("Returns list")
+    def test_returns_list(self, test_args):
+        relationships = get_other_relationships(**test_args)
+        assert isinstance(relationships, list)
+
+    @pytest.mark.it("Returns list with 2 dictinoaries")
+    def test_returns_list_of_two_dicts(self, test_args):
+        relationships = get_other_relationships(**test_args)
+        assert len(relationships) == 2
+        for r in relationships:
+            assert isinstance(r, dict)
+
+    @pytest.mark.it("Expected data on expected keys of dictioary")
+    def test_expected_data_on_expected_keys(self, test_args):
+        relationships = get_other_relationships(**test_args)
+        for r in relationships:
+            assert r["Party_Relationship_Eff_Date"] == "2025-07-07"
+            assert isinstance(r["PartyRelationship_ID"], dict)
+
+    @pytest.mark.it("Points to valid organisation ID")
+    def test_valid_org(self, test_args):
+        orgs = [10000, "02"]
+        relationships = get_other_relationships(**test_args)
+        for i, r in enumerate(relationships):
+            parties = r["PartyRelationship_ID"]
+            assert parties["Party_Id_1st"] == "JCQ"
+            assert parties["Party_Id_2nd"] == orgs[i]
