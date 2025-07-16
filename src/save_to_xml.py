f0e27c19f3e4583e5790053897dbb0c94fa09737
Jessica Marcell <marcellj@cityschools.org.uk>
2025-07-16 09:32:41 +0100

    implement write_xml_to_file function

--- src/save_to_xml.py
+++ src/save_to_xml.py
@@ -1,12 +1,32 @@
 from dict2xml import dict2xml, DataSorter
 
-def export_dict_to_xml(dict_to_convert: dict):
+
+def export_dict_to_xml(dict_to_convert: dict) -> str:
+    """Takes python dictinoary and exports to xml string.
+
+    None values in the dictionary will create self closing xml tags.
+    """
+
     data_sorter = DataSorter.never()
     return dict2xml(
         dict_to_convert,
-        data_sorter = data_sorter,
-        closed_tags_for = [None]
+        data_sorter=data_sorter,
+        closed_tags_for=[None]
     )
 
-def write_xml_to_file():
-    pass
+
+def write_xml_to_file(xml: str, filepath: str) -> None:
+    """Writes xml string to xml file.
+
+    Adds opening and closing tags from A2C file.
+    Args:
+        xml(str): xml string to be written to file.
+        filepath(str): relative path where file will be saved.
+    """
+
+    with open(filepath, "w") as f:
+        f.write('<A2CMessage xmlns:xs="http://www.w3.org/2001/XMLSchema" ')
+        f.write('xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ')
+        f.write('xmlns="http://jcq.org.uk/a2c">\n')
+        f.write(xml)
+        f.write('\n</A2CMessage>')
--- test/test_save_to_xml.py
+++ test/test_save_to_xml.py
@@ -1,5 +1,7 @@
 import pytest
 from src.save_to_xml import export_dict_to_xml, write_xml_to_file
+from os.path import isfile
+
 
 @pytest.mark.it("Test export_dict_to_xml function")
 class TestExportDictToXML:
@@ -7,7 +9,7 @@ class TestExportDictToXML:
     def test_returns_string(self):
         xml = export_dict_to_xml({})
         assert isinstance(xml, str)
-    
+
     @pytest.mark.it("Returns expected value")
     def test_returns_expected_value(self):
         test_dict = {
@@ -43,6 +45,39 @@ class TestExportDictToXML:
         result = export_dict_to_xml(test_dict)
         assert result == expected_value
 
+
 @pytest.mark.it("Test write_xml_to_file function")
 class TestWriteXMLToFile:
-    pass
+    @pytest.mark.it("Adds file to location")
+    def test_saves_file(self, tmp_path):
+        d = tmp_path / "sub"
+        d.mkdir()
+        p = d / "file.xml"
+        write_xml_to_file("", p)
+        assert isfile(p)
+
+    @pytest.mark.it("Adds opening and closing tags.")
+    def test_adds_opening_and_closing_tags(self, tmp_path):
+        expected_line_1 = (
+            '<A2CMessage xmlns:xs="http://www.w3.org/2001/XMLSchema" xmlns' +
+            ':xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http' +
+            '://jcq.org.uk/a2c">\n'
+        )
+        d = tmp_path / "sub"
+        d.mkdir()
+        p = d / "file.xml"
+        write_xml_to_file("", p)
+        with open(p, "r") as f:
+            assert f.readline() == expected_line_1
+            assert f.readline() == "\n"
+            assert f.readline() == '</A2CMessage>'
+
+    @pytest.mark.it("Writes expected content to the file")
+    def test_writes_expected_content_to_file(self, tmp_path):
+        test_xml = "<test>1</test>"
+        d = tmp_path / "sub"
+        d.mkdir()
+        p = d / "file.xml"
+        write_xml_to_file(test_xml, p)
+        with open(p, "r") as f:
+            assert test_xml in f.read()
