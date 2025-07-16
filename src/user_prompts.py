58a70771a1aba7ce2c73f493d0225ccfbc620662
Jessica Marcell <marcellj@cityschools.org.uk>
2025-07-16 11:11:52 +0100

    implement valid_headings and get_csv_filepath functions

--- src/user_prompts.py
+++ src/user_prompts.py
@@ -1,7 +1,7 @@
 from os.path import isfile
 
 
-def get_filepath():
+def get_excel_filepath() -> str:
     """Prompts the user for input and provides relative path to xlsx file.
 
     The function prompts the user to save file into cambridge-a2c/data folder
@@ -50,4 +50,72 @@ def get_filepath():
         # add file extension
         if len(filename) > 5 and filename[-5:] != ".xlsx":
             filename += ".xlsx"
+    return filepath + filename
+
+
+def valid_headings(filepath: str) -> bool:
+    """Takes csv file and returns True if contains "UCI", "Candidate Number"
+    and "Date of Birth" headings, False if not.
+
+    Args:
+        filepath(str): path to csv file to be checked
+    """
+    with open(filepath, "r", encoding="utf-8") as f:
+        headings = f.readline().rstrip().split(",")
+
+        if "Candidate Number" not in headings:
+            print(f"\nCandidate Number column not found in {headings}")
+            print("Please check spaces and capitalisation and retry.")
+            return False
+
+        elif "UCI" not in headings:
+            print(f"\nUCI column not found in {headings}")
+            print("Please check spaces and capitalisation and retry.")
+            return False
+
+        elif "Date of Birth" not in headings:
+            print(f"\nDate of Birth column not found in {headings}")
+            print("Please check spaces and capitalisation and retry.")
+            return False
+        return True
+
+
+def get_csv_filepath() -> str:
+    filepath = "data/"
+    print(
+        "Please save csv file with 'Candidate Number', 'UCI' and 'Date of " +
+        "Birth' headings to the cambridge-a2c/data folder.\nThis can include"
+        " data for all candidates or just those with Cambridge International "
+        "entries. Date of Birth column should be in dd/mm/yyyy format.\n" +
+        "Please ensure headings are exactly as above including " +
+        "capitalisation and spaces. If saving csv from Excel save to " +
+        "CSV UTF-8 (Comma Delimited) format"
+    )
+    filename = input("candidate csv filename: ")
+
+    # adds file extension if none provided
+    if len(filename) > 4 and filename[-4:] != ".csv":
+        filename += ".csv"
+
+    # ask for input again if file doesn't exist in cambridge-a2c/data folder
+    while (
+        not isfile(filepath + filename)
+        or not valid_headings(filepath + filename)
+    ):
+        if not isfile(filepath + filename):
+            print(
+                f"File {filename} not found in cambridge-a2c/data folder," +
+                " (only accepts .csv files)"
+            )
+        else:
+            print(
+                "Please save csv file with correct headings to " +
+                "cambridge-a2c/data folder"
+            )
+        filename = input("Please enter filename again: ")
+
+        # add file extension
+        if len(filename) > 4 and filename[-4:] != ".csv":
+            filename += ".csv"
+
     return filepath + filename
--- test/test_user_prompts.py
+++ test/test_user_prompts.py
@@ -1,40 +1,134 @@
 import pytest
 from unittest.mock import patch
-from src.user_prompts import get_filepath
+from src.user_prompts import (
+    get_excel_filepath,
+    get_csv_filepath,
+    valid_headings
+)
 
 
-@pytest.mark.it('Returns string')
-@patch("src.user_prompts.input", return_value="Testing.xlsx")
-def test_returns_str(mock_input):
-    """using patching to provide mock user input: Testing.xlsx"""
+@pytest.mark.it("Testing get_excel_filepath function")
+class TestGetExcelFilepath:
+    @pytest.mark.it('Returns string')
+    @patch("src.user_prompts.input", return_value="Testing.xlsx")
+    def test_returns_str(self, mock_input):
+        """using patching to provide mock user input: Testing.xlsx"""
 
-    filepath = get_filepath()
-    assert isinstance(filepath, str)
+        filepath = get_excel_filepath()
+        assert isinstance(filepath, str)
+
+    @pytest.mark.it('Returns expected path.')
+    @patch("src.user_prompts.input", return_value="Testing.xlsx")
+    def test_returns_expected_path(self, mock_input):
+        """using patching to provide mock user input: Testing.xlsx"""
+
+        filepath = get_excel_filepath()
+        assert filepath == "data/Testing.xlsx"
+
+    @pytest.mark.it('Adds file extension when not provided')
+    @patch("src.user_prompts.input", return_value="Testing")
+    def test_adds_file_extension(self, mock_input):
+        """using patching to provide mock user input: Testing"""
+
+        filepath = get_excel_filepath()
+        assert filepath == "data/Testing.xlsx"
+
+    @pytest.mark.it('Doesn\'t accept non-existent file')
+    @patch("src.user_prompts.input")
+    def test_non_existent_file(self, mock_input):
+        """using patching to provide a list of mock user inputs"""
+
+        mock_input.side_effect = ["Missing", "Testing.doc", "Testing.xlsx"]
+        filepath = get_excel_filepath()
+        assert filepath == "data/Testing.xlsx"
 
 
-@pytest.mark.it('Returns expected path.')
-@patch("src.user_prompts.input", return_value="Testing.xlsx")
-def test_returns_expected_path(mock_input):
-    """using patching to provide mock user input: Testing.xlsx"""
+class TestValidHeadings:
+    @pytest.mark.it("Returns true if correct headings")
+    def test_returns_true(self):
+        temp_file = "data/headings_test.csv"
+        with open(temp_file, "w") as f:
+            f.write("Candidate Number,UCI,Date of Birth\n")
+            f.write("new line")
+        assert valid_headings(temp_file)
 
-    filepath = get_filepath()
-    assert filepath == "data/Testing.xlsx"
+    @pytest.mark.it("Returns True if additional headings")
+    def test_returns_true_with_additional_headings(self):
+        temp_file = "data/headings_test.csv"
+        with open(temp_file, "w") as f:
+            f.write("Heading3,Candidate Number,UCI,Heading,Date of Birth\n")
+            f.write("new line")
+        assert valid_headings(temp_file)
+
+    @pytest.mark.it("Returns False if no UCI")
+    def test_returns_false_with_no_uci(self):
+        temp_file = "data/headings_test.csv"
+        with open(temp_file, "w") as f:
+            f.write("Heading3,Candidate Number,Heading,Date of Birth\n")
+            f.write("new line")
+        assert not valid_headings(temp_file)
+
+    @pytest.mark.it("Returns False if different capitalisation")
+    def test_returns_false_with_no_different_capitalisation(self):
+        temp_file = "data/headings_test.csv"
+        with open(temp_file, "w") as f:
+            f.write("candidate number,UCI,Date of Birth\n")
+            f.write("new line")
+        assert not valid_headings(temp_file)
+
+    @pytest.mark.it("Returns False if different white space")
+    def test_returns_false_if_different_white_space(self):
+        temp_file = "data/headings_test.csv"
+        with open(temp_file, "w") as f:
+            f.write("Candidate Number,UCI, Date of Birth\n")
+            f.write("new line")
+        assert not valid_headings(temp_file)
 
 
-@pytest.mark.it('Adds file extension when not provided')
-@patch("src.user_prompts.input", return_value="Testing")
-def test_adds_file_extension(mock_input):
-    """using patching to provide mock user input: Testing"""
+@pytest.mark.it("Testing get_excel_filepath function")
+class TestGetCSVFilepath:
+    @pytest.mark.it('Returns string')
+    @patch("src.user_prompts.input", return_value="candidates.csv")
+    def test_returns_str(self, mock_input):
+        """using patching to provide mock user input: candidates.csv"""
 
-    filepath = get_filepath()
-    assert filepath == "data/Testing.xlsx"
+        filepath = get_csv_filepath()
+        assert isinstance(filepath, str)
 
+    @pytest.mark.it('Returns expected path.')
+    @patch("src.user_prompts.input", return_value="candidates.csv")
+    def test_returns_expected_path(self, mock_input):
+        """using patching to provide mock user input: candidates.csv"""
 
-@pytest.mark.it('Doesn\'t accept non-existent file')
-@patch("src.user_prompts.input")
-def test_non_existent_file(mock_input):
-    """using patching to provide a list of mock user inputs defined below"""
+        filepath = get_csv_filepath()
+        assert filepath == "data/candidates.csv"
 
-    mock_input.side_effect = ["Missing", "Testing.doc", "Testing.xlsx"]
-    filepath = get_filepath()
-    assert filepath == "data/Testing.xlsx"
+    @pytest.mark.it('Adds file extension when not provided')
+    @patch("src.user_prompts.input", return_value="candidates")
+    def test_adds_file_extension(self, mock_input):
+        """using patching to provide mock user input: candidates"""
+
+        filepath = get_csv_filepath()
+        assert filepath == "data/candidates.csv"
+
+    @pytest.mark.it('Doesn\'t accept non-existent file')
+    @patch("src.user_prompts.input")
+    def test_non_existent_file(self, mock_input):
+        """using patching to provide a list of mock user inputs"""
+
+        mock_input.side_effect = ["Missing", "Testing.xlsx", "candidates.csv"]
+        filepath = get_csv_filepath()
+        assert filepath == "data/candidates.csv"
+
+    @pytest.mark.it("Doesn't accept file with different headings")
+    @patch("src.user_prompts.input")
+    def test_invalid_headings(self, mock_input):
+        """using patching to provide a list of mock user inputs"""
+
+        temp_file = "headings_test.csv"
+
+        with open("data/" + temp_file, "w") as f:
+            f.write("candidate number,uci,dat of birth")
+        mock_input.side_effect = [temp_file, "candidates.csv"]
+        filepath = get_csv_filepath()
+        assert filepath == "data/candidates.csv"
